a3609899cc721e8e68cbb4d2df8f821c7bc9fe39
Vladon-2307 <vladon.23.07.2001@gmail.com>
2021-02-22 08:44:44 +0300

    Lamda_Colection first

---
+++ Lab_1/Lamda_Colection/src/Lamda_Colection/data/Lamda_Colection.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lamda_Colection.data;
+
+/**
+ *
+ * @author vlado
+ */
+public class Lamda_Colection {
+    public static void Z_1(){
+        
+    }
+}
