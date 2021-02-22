0cec1196767c86902fe0f866c6b462d3eacd0324
Vladon-2307 <vladon.23.07.2001@gmail.com>
2021-02-22 09:34:18 +0300

    Lamda_Colection first

--- Lab_1/Lamda_Colection/src/Lamda_Colection/console/main.java
+++ Lab_1/Lamda_Colection/src/Lamda_Colection/console/main.java
@@ -5,12 +5,17 @@
  */
 package Lamda_Colection.console;
 
+import Lamda_Colection.data.Lamda_Colection;
+
 /**
  *
  * @author vlado
  */
 public class main {
+
     public static void main(String[] args) {
-        
+        Lamda_Colection.Z_1();
+        Lamda_Colection.Z_2();
+        Lamda_Colection.Z_3();
     }
 }
--- Lab_1/Lamda_Colection/src/Lamda_Colection/data/Lamda_Colection.java
+++ Lab_1/Lamda_Colection/src/Lamda_Colection/data/Lamda_Colection.java
@@ -5,12 +5,52 @@
  */
 package Lamda_Colection.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author vlado
  */
 public class Lamda_Colection {
-    public static void Z_1(){
-        
+
+    public static void Z_1() {
+        List<Integer> list = new ArrayList<>();
+        list.add(12);
+        list.add(-3);
+        list.add(6);
+        list.add(-8);
+        list.add(0);
+        Integer max = list.stream().reduce(Integer::max).get();
+        Integer min = list.stream().reduce(Integer::min).get();
+        System.out.println("Max= " + max + " Min= " + min);
+    }
+
+    public static void Z_2() {
+        List<Integer> list = new ArrayList<>();
+        list.add(12);
+        list.add(-3);
+        list.add(-6);
+        list.add(-8);
+        list.add(0);
+        list.add(3);
+        list.add(6);
+        list.add(8);
+        Integer min = list.stream().filter(x -> x < 0).reduce((a, b) -> Integer.min(a, b)).get();
+        System.out.println("Минимальное отрицательное число= " + min);
+    }
+    
+    public static void Z_3(){
+        List<String> list = new ArrayList<>();
+        list.add("122");
+        list.add("33");
+        list.add("62");
+        list.add("83");
+        list.add("111");
+        list.add("3232");
+        list.add("632");
+        list.add("832");
+        int MAX = list.stream().mapToInt((s) -> Integer.parseInt(s)).sum();
+        System.out.println(MAX);
     }
 }
