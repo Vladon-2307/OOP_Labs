619f96f86fe3155667b5e6dd2cca9b9a16acd2e7
Vladon-2307 <vladon.23.07.2001@gmail.com>
2021-02-23 22:23:10 +0300

    Lamda_Colection first

--- Lab_1/Lamda_Second/src/Lamda_Second/data/Lamda_Second.java
+++ Lab_1/Lamda_Second/src/Lamda_Second/data/Lamda_Second.java
@@ -6,7 +6,9 @@
 package Lamda_Second.data;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.IntPredicate;
 
 /**
  *
@@ -16,55 +18,49 @@ public class Lamda_Second {
 
     public static void Z_1() {
         String s = "AAAccDDDccW";
-
-        int upperCase = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (Character.isUpperCase(s.charAt(i))) {
-                upperCase++;
+        int upperCase;
+        upperCase = (int) s.chars().sorted().filter(new IntPredicate() {
+            @Override
+            public boolean test(int s) {
+                return s >= 65 && s <= 90;
             }
-        }
+        }).count();
         System.out.println("В этом предложении общее КОЛИЧЕСТВО строчних букв =  " + upperCase);
     }
 
     public static void Z_2() {
-        String[] Spisoc = {"Abs", "sder", "aer", "aert", "der"};
-        for (String Spisoc1 : Spisoc) {
-            if (Spisoc1.indexOf("A") == 0 && Spisoc1.length() == 3) {
-                System.out.println(Spisoc1);
-            }
-        }
+        String[] Spisoc = {"Abs", "sder", "aer", "art", "der"};
+        Arrays.stream(Spisoc).filter(s -> s.indexOf("a") == 0 && s.length() == 3).forEachOrdered(s -> System.out.println(s + " "));
+
+        //System.out.println(Rezult);
     }
 
     public static void Z_3() {
         List<String> Spisok = new ArrayList<>();
-        Spisok.add("ASdasa");
+        Spisok.add("ASdaa");
         Spisok.add("ASwew");
         Spisok.add("CCVdww");
         Spisok.add("DWSDDS");
         Spisok.add("ASdDDDadsa");
-        int index = 0;
-        if (!Spisok.isEmpty()) {
-            for (int i = 0; i < Spisok.size(); i++) {
-                int upperCase1 = 0;
-                int upperCase2 = 0;
-                for (int y = 0; y < Spisok.get(index).length(); y++) {
-                    if (Character.isUpperCase(Spisok.get(index).charAt(y))) {
-                        upperCase1++;
-                    }
-                }
-                for (int z = 0; z < Spisok.get(i).length(); z++) {
-                    if (Character.isUpperCase(Spisok.get(i).charAt(z))) {
-                        upperCase2++;
 
-                    }
-                }
-                if (upperCase1 < upperCase2) {
-                    index = i;
-                }
+        System.out.println(Spisok.stream().sorted((o1, o2) -> {
+            int a1 = UperCase(o1);
+            int a2 = UperCase(o2);
+            if(a1>a2)return 1;
+            else if(a1<a2)return -1;
+            else return 0;
+        }).findFirst().get());
+    }
+
+    public static int UperCase(String s) {
+        
+        int upperCase;
+        upperCase = (int) s.chars().sorted().filter(new IntPredicate() {
+            @Override
+            public boolean test(int s) {
+                return s >= 65 && s <= 90;
             }
-            System.out.println(Spisok.get(index));
-        } else {
-            System.err.println("Список пуст!");
-        }
+        }).count();
+        return upperCase;
     }
 }
