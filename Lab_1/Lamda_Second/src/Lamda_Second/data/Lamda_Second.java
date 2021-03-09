ed7e44c3ca0e5b993aaf4b2f426940d5ccaa1f29
Vladon-2307 <vladon.23.07.2001@gmail.com>
2021-03-09 14:18:17 +0300

    Lamda_second

--- Lab_1/Lamda_Second/src/Lamda_Second/data/Lamda_Second.java
+++ Lab_1/Lamda_Second/src/Lamda_Second/data/Lamda_Second.java
@@ -18,13 +18,7 @@ public class Lamda_Second {
 
     public static void Z_1() {
         String s = "AAAccDDDccW";
-        int upperCase;
-        upperCase = (int) s.chars().sorted().filter(new IntPredicate() {
-            @Override
-            public boolean test(int s) {
-                return s < 65 || s > 90;
-            }
-        }).count();
+        long upperCase = s.chars().filter(Character::isLowerCase).count();
         System.out.println("В этом предложении общее количество строчных букв =  " + upperCase);
     }
 
@@ -44,18 +38,18 @@ public class Lamda_Second {
         Spisok.add("ASdDDDadsa");
 
         System.out.println(Spisok.stream().sorted((o1, o2) -> {
-            int a1 = UperCase(o1);
-            int a2 = UperCase(o2);
+            long a1 = UperCase(o1);
+            long a2 = UperCase(o2);
             if(a1>a2)return 1;
             else if(a1<a2)return -1;
             else return 0;
         }).findFirst().get());
     }
 
-    public static int UperCase(String s) {
+    public static long UperCase(String s) {
         
-        int upperCase;
-        upperCase = (int) s.chars().sorted().filter((int s1) -> s1 <= 65 || s1 >= 90).count();
+        long upperCase;
+        upperCase = s.chars().filter(Character::isLowerCase).count();
         return upperCase;
     }
 }
