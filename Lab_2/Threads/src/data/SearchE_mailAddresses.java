8bd7e536b9e34e601786ab9d8d91d8c6bfc74139
Vladon-2307 <vladon.23.07.2001@gmail.com>
2021-04-06 13:56:14 +0300

    Threads

---
+++ Lab_2/Threads/src/data/SearchE_mailAddresses.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import java.util.List;
+
+/**
+ *
+ * @author vlado
+ */
+public class SearchE_mailAddresses {
+    private List<String> theWay;
+    
+    private void search(String way){
+        
+    }
+    
+}
