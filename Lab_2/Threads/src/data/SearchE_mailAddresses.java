178ac3ec28b33aed97db06f8838c37f9a3e5fb73
Vladon-2307 <vladon.23.07.2001@gmail.com>
2021-04-08 11:38:43 +0300

    Threads

--- Lab_2/Threads/src/data/SearchE_mailAddresses.java
+++ Lab_2/Threads/src/data/SearchE_mailAddresses.java
@@ -5,6 +5,12 @@
  */
 package data;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,10 +18,56 @@ import java.util.List;
  * @author vlado
  */
 public class SearchE_mailAddresses {
-    private List<String> theWay;
-    
-    private void search(String way){
-        
+
+    private List<String> theWay = new ArrayList<>();
+
+    private void search(String way) {
+        List<String> rezult = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File(way)));
+            String line = reader.readLine();
+            while (line != null) {
+                String sech[] = line.split(" ");
+                for (String sech1 : sech) {
+                    if (sech1.contains("@")) {
+                        rezult.add(sech1);
+                    }
+                }
+                line = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println(Thread.currentThread().getName() + ": файл не найден");
+            return;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (rezult.isEmpty()) {
+            System.out.println(Thread.currentThread().getName() + ": e-mail адреса отсуствуют");
+        } else {
+            System.out.println(Thread.currentThread().getName() + ": e-mail адреса: " + rezult.toString());
+        }
     }
-    
+
+    public void addFile(String URL) {
+        theWay.add(URL);
+    }
+
+    public void startSearch() {
+        List<Thread> ts = new ArrayList<>();
+        theWay.stream().map((url) -> new Thread(() -> {
+            search(url);
+        })).map((t) -> {
+            ts.add(t);
+            return t;
+        }).forEachOrdered((t) -> {
+            t.start();
+        });
+        ts.stream().forEachOrdered(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        });
+    }
 }
--- Lab_2/Threads/src/main/Main.java
+++ Lab_2/Threads/src/main/Main.java
@@ -5,13 +5,31 @@
  */
 package main;
 
+import data.*;
+import java.util.Scanner;
+
 /**
  *
  * @author vlado
  */
 public class Main {
+    
+    public static void z1(){
+        SearchE_mailAddresses S = new SearchE_mailAddresses();
+        Scanner in = new Scanner(System.in);
+        String url = in.nextLine();
+        while(!url.equals("")){
+            S.addFile(url);
+            url = in.nextLine();
+        }
+//        S.addFile("E:\\OOP\\Labs\\Lab_2\\Threads\\src\\Files\\t1.txt");
+//        S.addFile("E:\\OOP\\Labs\\Lab_2\\Threads\\src\\Files\\t2.txt");
+//        S.addFile("E:\\OOP\\Labs\\Lab_2\\Threads\\src\\Files\\t3.txt");
+//        S.addFile("E:\\OOP\\Labs\\Lab_2\\Threads\\src\\Files\\non.txt");
+        S.startSearch();
+    }
 
     public static void main(String[] args) {
-
+        z1();
     }
 }
